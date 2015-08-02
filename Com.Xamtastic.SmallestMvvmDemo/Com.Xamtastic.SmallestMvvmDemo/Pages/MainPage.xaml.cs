6879a9ce49152492742ece15dc6917fa9efc0495
Anthony Harrison <anthony.harrison@xamtastic.com>
2015-08-02 10:17:45 +0100

    Initial Commit.

---
+++ Com.Xamtastic.SmallestMvvmDemo/Com.Xamtastic.SmallestMvvmDemo/Pages/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Com.Xamtastic.Patterns.SmallestMvvm;
+using Com.Xamtastic.SmallestMvvmDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Com.Xamtastic.SmallestMvvmDemo.Pages
+{
+    [ViewModelType(typeof(MainPageViewModel))]
+    public partial class MainPage : PageBase
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Com.Xamtastic.SmallestMvvmDemo/Com.Xamtastic.SmallestMvvmDemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Com.Xamtastic.Patterns.SmallestMvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Xamtastic.SmallestMvvmDemo.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _message = "Hello SmallestMvvm!";
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+    }
+}
